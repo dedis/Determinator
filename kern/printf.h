3953770ca409e8a3dbd2335c2ed2c925eb6991f8
rsc <rsc>
2003-09-29 21:14:55 +0000

    Turn off format checking so we can use our extensions. Damn gcc..

--- kern/printf.h
+++ kern/printf.h
@@ -3,13 +3,18 @@
 #ifndef _PRINTF_H_
 #define _PRINTF_H_
 
+/*
+ * Sigh.  In order to use our own extensions, we have to give up
+ * format-checking from gcc. 
+ */
+
 void warn(const char *, ...)
-	__attribute__((format(printf, 1, 2)));
+	/* __attribute__((format(printf, 1, 2))) */;
 void _panic(const char *, int, const char *, ...) 
 	__attribute__((noreturn))
-	__attribute__((format(printf, 3, 4)));
+	/* __attribute__((format(printf, 3, 4))) */;
 int printf(const char *, ...)
-	__attribute__((format(printf, 1, 2)));
+	/* __attribute__((format(printf, 1, 2))) */;
 
 #define assert(x)	\
 	do {	if (!(x)) panic("assertion failed: %s", #x); } while (0)
