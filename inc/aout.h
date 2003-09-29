e2b6fc3711768f929a81fc7e22c8a29791e58435
rsc <rsc>
2003-09-29 20:04:30 +0000

    Add aout.h for lab3+.

---
+++ inc/aout.h
@@ -0,0 +1,17 @@
+///LAB3
+#ifndef _AOUT_H_
+#define _AOUT_H_ 1
+
+struct a_out_hdr {
+	u_long	a_midmag;	/* flags<<26 | mid<<16 | magic */
+	u_long	a_text;		/* text segment size */
+	u_long	a_data;		/* initialized data size */
+	u_long	a_bss;		/* uninitialized data size */
+	u_long	a_syms;		/* symbol table size */
+	u_long	a_entry;	/* entry point */
+	u_long	a_trsize;	/* text relocation size */
+	u_long	a_drsize;	/* data relocation size */
+};
+
+#endif
+///END
