36bbf5e27a0a9f5c68f9f0ab37690437fdbec9f8
rsc <rsc>
2003-09-29 20:58:35 +0000

    Add NERRORS.

--- inc/error.h
+++ inc/error.h
@@ -4,7 +4,7 @@
 #ifndef _ERROR_H_
 #define _ERROR_H_
 
-/* Kernel error codes */
+/* Kernel error codes -- keep in sync with list in kern/printf.c. */
 #define E_UNSPECIFIED	1	/* Unspecified or unknown problem */
 #define E_BAD_ENV       2       /* Environment doesn't exist or otherwise
 				   cannot be used in requested action */
@@ -13,6 +13,8 @@
 #define E_NO_FREE_ENV   5       /* Attempt to create a new environment beyond
 				   the maximum allowed */
 #define E_IPC_BLOCKED   6       /* Attempt to ipc to env blocking ipc's */
+
+#define NERRORS	7
 
 #endif /* _ERROR_H_ */
 ///END
