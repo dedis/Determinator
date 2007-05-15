0be0835e97764d996cd6161f1b57ccef134ec4cf
rsc <rsc>
2007-05-15 14:12:02 +0000

    oops - forgot to take pp0 back out of boot_pgdir

--- kern/pmap.c
+++ kern/pmap.c
@@ -971,6 +971,8 @@ page_check(void)
 	ptep = page2kva(pp0);
 	for(i=0; i<NPTENTRIES; i++)
 		assert((ptep[i] & PTE_P) == 0);
+	boot_pgdir[PDX(va)] = 0;
+	pp0->pp_ref = 0;
 
 	// give free list back
 	page_free_list = fl;
