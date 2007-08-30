3a8d9d04c3c93d87cb102878351ab7f144911100
rsc <rsc>
2007-08-30 23:48:34 +0000

    force binary mode i/o (fixes a problem for Rose Liu)

--- boot/sign.pl
+++ boot/sign.pl
@@ -1,8 +1,11 @@
-#!/bin/perl
+#!/usr/bin/perl
 
-open(SIG, $ARGV[0]) || die "open $ARGV[0]: $!";
+open(BB, $ARGV[0]) || die "open $ARGV[0]: $!";
 
-$n = sysread(SIG, $buf, 1000);
+binmode BB;
+my $buf;
+read(BB, $buf, 1000);
+$n = length($buf);
 
 if($n > 510){
 	print STDERR "boot block too large: $n bytes (max 510)\n";
@@ -14,6 +17,7 @@ print STDERR "boot block is $n bytes (max 510)\n";
 $buf .= "\0" x (510-$n);
 $buf .= "\x55\xAA";
 
-open(SIG, ">$ARGV[0]") || die "open >$ARGV[0]: $!";
-print SIG $buf;
-close SIG;
+open(BB, ">$ARGV[0]") || die "open >$ARGV[0]: $!";
+binmode BB;
+print BB $buf;
+close BB;
