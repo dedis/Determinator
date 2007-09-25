d62b46e2b320d8f2f7a175ea71c951eefde9af01
rtm <rtm>
2007-09-25 01:02:24 +0000

    don't suggest using pgdir_walk in user_mem_check()

--- kern/pmap.c
+++ kern/pmap.c
@@ -819,8 +819,6 @@ static uintptr_t user_mem_check_addr;
 // Returns 0 if the user program can access this range of addresses,
 // and -E_FAULT otherwise.
 //
-// Hint: The TA solution uses pgdir_walk.
-//
 int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
