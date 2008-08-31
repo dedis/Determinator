7ce3620d76b1bac6ca96a9ce96c062ef8657522c
rtm <rtm>
2008-08-31 09:36:49 +0000

    don't require that they find # args in ELF debugging info

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -246,16 +246,11 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 
 	// Set eip_fn_narg to the number of arguments taken by the function,
 	// or 0 if there was no containing function.
-#if SOL >= 1
 	if (lfun < rfun)
 		for (lline = lfun + 1;
 		     lline < rfun && stabs[lline].n_type == N_PSYM;
 		     lline++)
 			info->eip_fn_narg++;
-#else
-	// Your code here.
-#endif
-
 	
 	return 0;
 }
