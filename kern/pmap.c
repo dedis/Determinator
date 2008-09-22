04acace80df6a3bc84d51d54a739769d541188ec
kolya <kolya>
2008-09-22 08:20:56 +0000

    fix thanks to Amrik S. Kochhar <amrik@mit.edu>

--- kern/pmap.c
+++ kern/pmap.c
@@ -418,7 +418,7 @@ check_boot_pgdir(void)
 #endif
 
 	// check phys mem
-	for (i = 0; i < npage; i += PGSIZE)
+	for (i = 0; i < npage * PGSIZE; i += PGSIZE)
 		assert(check_va2pa(pgdir, KERNBASE + i) == i);
 
 	// check kernel stack
