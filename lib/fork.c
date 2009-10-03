31234642a55d49b51b5bc7fe9b2437a5dc936b78
Austin Clements <amdragon@mit.edu>
2009-10-03 02:16:46 -0400

    The comment about remapping COW pages as COW again may or may not apply depending on how you do things.  Try to clarify a bit.

--- lib/fork.c
+++ lib/fork.c
@@ -71,8 +71,9 @@ pgfault(struct UTrapframe *utf)
 // Map our virtual page pn (address pn*PGSIZE) into the target envid
 // at the same virtual address.  If the page is writable or copy-on-write,
 // the new mapping must be created copy-on-write, and then our mapping must be
-// marked copy-on-write as well.  (Exercise: Why mark ours copy-on-write again
-// if it was already copy-on-write?)
+// marked copy-on-write as well.  (Exercise: Why might we need to mark ours
+// copy-on-write again if it was already copy-on-write at the beginning of
+// this function?)
 //
 // Returns: 0 on success, < 0 on error.
 // It is also OK to panic on error.
