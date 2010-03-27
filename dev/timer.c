b755f0a561c8a06033a7a0f669eb84e76f5bb071
Bryan Ford <brynosaurus@gmail.com>
2010-03-27 19:09:51 -0400

    Resurrect the old PIT-related files from JOS

---
+++ dev/timer.c
@@ -0,0 +1,51 @@
+#if LAB >= 2	// pmap.c must read NVRAM to detect how much memory we have
+/* See COPYRIGHT for copyright information. */
+
+/* Support for two time-related hardware gadgets: 1) the run time
+ * clock with its NVRAM access functions; 2) the 8253 timer, which
+ * generates interrupts on IRQ 0.
+ */
+
+#include <inc/x86.h>
+#if LAB >= 4
+#include <inc/stdio.h>
+#include <inc/isareg.h>
+#include <inc/timerreg.h>
+#endif
+
+#include <kern/kclock.h>
+#if LAB >= 4
+#include <kern/picirq.h>
+#endif
+
+
+unsigned
+mc146818_read(unsigned reg)
+{
+	outb(IO_RTC, reg);
+	return inb(IO_RTC+1);
+}
+
+void
+mc146818_write(unsigned reg, unsigned datum)
+{
+	outb(IO_RTC, reg);
+	outb(IO_RTC+1, datum);
+}
+
+
+#if LAB >= 4	// when we actually start needing the timers
+void
+kclock_init(void)
+{
+	/* initialize 8253 clock to interrupt 100 times/sec */
+	outb(TIMER_MODE, TIMER_SEL0 | TIMER_RATEGEN | TIMER_16BIT);
+	outb(IO_TIMER1, TIMER_DIV(100) % 256);
+	outb(IO_TIMER1, TIMER_DIV(100) / 256);
+	cprintf("	Setup timer interrupts via 8259A\n");
+	irq_setmask_8259A(irq_mask_8259A & ~(1<<0));
+	cprintf("	unmasked timer interrupt\n");
+}
+#endif /* LAB >= 4 */
+
+#endif /* LAB >= 2 */
