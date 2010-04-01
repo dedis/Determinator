5cd7fc4c8513c4cd2df5a46d4447be894b11284e
Bryan Ford <brynosaurus@gmail.com>
2010-03-31 23:01:32 -0400

    message structs

--- kern/net.h
+++ kern/net.h
@@ -8,16 +8,17 @@
 #endif
 
 // Ethernet header
-typedef struct net_ehdr {
+typedef struct net_ethhdr {
 	uint8_t		dst[6];		// Destination MAC address
 	uint8_t		src[6];		// Source MAC address
 	uint16_t	etype;		// Ethernet packet type
-} net_ehdr;
+} net_ethhdr;
 
 #define NET_ETYPE_IP	0x0800		// Ethernet packet type for IPv4
 
 #define NET_MAXPKT	1514		// Max Ethernet packet size
 
+#if LAB >= 99
 // Internet Protocol (IP) header - see RFC791
 typedef struct net_iphdr {
 	uint8_t		ver_ihl;	// Version and Internet Header Length
@@ -56,6 +57,55 @@ typedef struct net_dhcphdr {
 	uint8_t		chaddr[16];	// Client hardware address
 	char		sname[64];	// Server host name
 } net_dhcphdr;
+#endif
+
+
+// Message types
+typedef enum net_msgtype {
+	NET_INVALID	= 0,
+	NET_STATUS,		// Message announcing our existence and status
+	NET_MIGRATEREQ,
+	NET_MIGRATEREPLY,
+	NET_FAULTREQ,
+	NET_FAULTREPLY,
+} net_msgtype;
+
+typedef struct net_statusmsg {
+	net_ethhdr	eth;
+	net_msgtype	type;	// = NET_STATUS
+	int		load;	// number of procs in our ready queue
+};
+
+typedef struct net_migratereq {
+	net_ethhdr	eth;
+	net_msgtype	type;	// = NET_MIGRATEREQ
+	uint32_t	proc;	// Origin node and addr in origin of proc
+	trapframe	tf;	// general registers
+	fxsave		fx;	// FPU/MMX/XMM state
+};
+
+typedef struct net_migreatreply {
+	net_ethhdr	eth;
+	net_msgtype	type;	// = NET_MIGRATEREPLY
+	int		orig;	// Origin node number
+	uint32_t	proc;	// Phys addr of proc struct on origin node
+};
+
+typedef struct net_faultreq {
+	net_ethhdr	eth;
+	net_msgtype	type;	// = NET_FAULTREQ
+	int		orig;	// Origin node number
+	uint32_t	proc;	// Phys addr of page on origin node
+};
+
+typedef struct net_faultreply {
+	net_ethhdr	eth;
+	net_msgtype	type;	// = NET_FAULTREPLY
+	int		orig;	// Origin node number
+	uint32_t	proc;	// Phys addr of page on origin node
+	int		part;	// Which part of the page this is: 0, 1, or 2
+	char		data[1400];
+};
 
 
 void net_init(void);
